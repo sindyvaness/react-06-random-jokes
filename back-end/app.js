410e45a76d76dcecafde065f3429d5bd11f22164
sindyvaness <sindyvaness@gmail.com>
2025-04-01 08:19:44 -0700

    creating the backend post api endpoint

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/playlists', (req, res) => {
+    res.status(200)
+                .send("success");
+});
+
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
+
